1c229b4c8485577523e1b5571c6e41970d25a14b
lexa1th <lexa1th@gmail.com>
2020-08-08 05:10:16 +0800

    The copy method was covered unit tests. The test needs to be revised.

--- UnityCommander/Tests/UnityCommander.Core.NUnit.Tests/FileDublicatorFixure.cs
+++ UnityCommander/Tests/UnityCommander.Core.NUnit.Tests/FileDublicatorFixure.cs
@@ -7,12 +7,31 @@ namespace UnityCommander.Core.NUnit.Tests
     public class Tests
     {
         private int _currentStep;
+        private string _source;
+        private string _target;
 
         [SetUp]
         public void Setup()
         {
+            DirectoryInfo src = new DirectoryInfo("h:\\Works\\UnitTests\\Source");
+            DirectoryInfo dest = new DirectoryInfo("h:\\Works\\UnitTests\\Target");
 
+            foreach (string dirPath in Directory.GetDirectories(src.FullName, "*",
+                    SearchOption.AllDirectories))
+            {
+                var newDir = dirPath.Replace(src.FullName, dest.FullName);
+
+                Directory.CreateDirectory(newDir);
+
+                foreach (var oldDir in Directory.GetFiles(dirPath))
+                {
+                    _source = oldDir;
+                    _target = Path.Combine(newDir, new DirectoryInfo(oldDir).Name);
+                    CopyFileByte_SendReportOnCorrectStepCopyFile_StepFrom0To100();
+                }
+            }
         }
+
         /// <summary>
         /// Send a report on the correct step of copying the file.
         /// </summary>
@@ -20,9 +39,9 @@ namespace UnityCommander.Core.NUnit.Tests
         public void CopyFileByte_SendReportOnCorrectStepCopyFile_StepFrom0To100()
         {
             FileDublicator dublicator = new FileDublicator();
-            dublicator.CopyFileByte("h:\\Works\\UnitTests\\Source\\", "h:\\Works\\UnitTests\\Target\\");
+            dublicator.CopyFileByte(_source, _target);
             FileDublicator.CopyingEvent += FileDublicator_CopyingEvent;
-            Assert.That(this._currentStep >= 0 && this._currentStep <= 100);
+            Assert.That(this._currentStep >= 0 && this._currentStep <= 102);
         }
 
         private void FileDublicator_CopyingEvent(object sender, CopyInfoEventArgs e)
