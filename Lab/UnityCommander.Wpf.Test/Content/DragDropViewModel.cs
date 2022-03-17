7b908e79ec454484c3de2d6fbe246cb47f37a1c9
lexa1th <lexa1th@gmail.com>
2022-03-17 10:12:01 +0800

    Working on file operation. Commit 3.

---
+++ Lab/UnityCommander.Wpf.Test/Content/DragDropViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityCommander.Wpf.Test.Content
+{
+    class DragDropViewModel
+    {
+    }
+}
---
+++ Lab/UnityCommander.Wpf.Test/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using UnityCommander.Helper.Mvvm.Base;
+
+namespace UnityCommander.Wpf.Test
+{
+    using Content;
+
+    public class MainViewModel : PropertiesChanged
+    {
+        private UserControl selectedCurrentTest;
+
+        private UserControl testContent;
+
+        public MainViewModel()
+        {
+            var copyFile = new CopyFileControl();
+            this.CollectionTests = new List<UserControl>
+            {
+                copyFile,
+                new DragDropControl()
+            };
+
+            this.TestContent = copyFile;
+        }
+
+        public List<UserControl> CollectionTests { get; set; }
+
+        public UserControl SelectedCurrentTest
+        {
+            get => this.selectedCurrentTest;
+            set
+            {
+                this.selectedCurrentTest = value;
+                this.TestContent = this.selectedCurrentTest;
+                this.OnPropertyChanged("SelectedCurrentTest");
+            }
+        }
+
+        public UserControl TestContent
+        {
+            get => this.testContent;
+            set 
+            {
+                this.testContent = value;
+                this.OnPropertyChanged("TestContent"); 
+            }
+        }
+    }
+}
